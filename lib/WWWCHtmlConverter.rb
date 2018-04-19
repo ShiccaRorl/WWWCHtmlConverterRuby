fdabd60db7b0dcc81b16d17a39d6ebdb137c0bbc
ShiccaRorl <shicca.rorl@gmail.com>
2018-04-18 22:36:32 -0700

    2018-04-19

--- lib/WWWCHtmlConverter.rb
+++ lib/WWWCHtmlConverter.rb
@@ -1,21 +1,17 @@
 # -*- encoding: utf-8 -*-
 
 require "WWWCHtmlConverter/version"
-require "WWWCHtmlConverter/WWWCHtmlConverterLib"
-require 'tk'
+#require "WWWCHtmlConverter/WWWCHTMLConverterLib"
+require "tk"
 
 module WWWCHtmlConverter
 
   # Your code goes here...
 
 
-  def tk()
-
-
-  end
 end
 
-wwwc_html_converter = WWWCHTMLConverter::WWWCHTMLConverter.new
+#wwwc_html_converter = WWWCHTMLConverter::WWWCHTMLConverter.new
 
 
 lavel_1 = TkLabel.new(text: "WWWC path")
